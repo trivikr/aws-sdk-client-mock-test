6bed8beaed63bcd8b8933776b44fd7ec8c8ee761
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-14 06:26:09 -0700

    chore: experimental code to add test

---
+++ src/index.spec.js
@@ -0,0 +1,18 @@
+import { mockClient } from "aws-sdk-client-mock";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
+
+describe("index", () => {
+  beforeEach(() => {
+    ddbMock.reset();
+  });
+
+  it("should get user names from the DynamoDB", async () => {
+    ddbMock.on(GetCommand).resolves({
+      Item: { id: "user1", name: "John" },
+    });
+    const names = await getUserNames(["user1"]);
+    expect(names).toStrictEqual(["John"]);
+  });
+});
